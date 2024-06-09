a8addbe02f1088289c70bd5767ef984964803c1d
HugoHamonangan <naberius103@gmail.com>
2024-06-10 02:13:16 +0700

    fixing all the bug and another adjustment

--- src/pages/QuizPage.tsx
+++ src/pages/QuizPage.tsx
@@ -8,10 +8,6 @@ import CryptoJS from 'crypto-js';
 import { useAppDispatch } from '../states/hooks/hooks';
 import { setIsUserPlayingNo } from '../states/slices/isUserPlayingSlices';
 
-interface QuizProps {
-  playAPI: string;
-}
-
 interface QuestionAndAnswer {
   question: string;
   shuffledAnswers: string[];
@@ -33,15 +29,20 @@ const shuffle = (array: string[]): string[] => {
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex--;
 
-    [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
   }
 
   return array;
 };
 
-const QuizPage: React.FC<QuizProps> = () => {
+const QuizPage: React.FC = () => {
   const [loading, setLoading] = useState<boolean>(true);
-  const [questionsAndAnswers, setQuestionsAndAnswers] = useState<QuestionAndAnswer[]>([]);
+  const [questionsAndAnswers, setQuestionsAndAnswers] = useState<
+    QuestionAndAnswer[]
+  >([]);
   const [warning, setWarning] = useState<boolean>(false);
   const [numCorrectAnswer, setNumCorrectAnswer] = useState(0);
   const [showResult, setShowResult] = useState<boolean>(false);
@@ -61,7 +62,9 @@ const QuizPage: React.FC<QuizProps> = () => {
     const fetchData = async () => {
       try {
         const encryptedData = localStorage.getItem('quizData');
-        const savedAnswers = JSON.parse(localStorage.getItem('selectedAnswers') || '[]');
+        const savedAnswers = JSON.parse(
+          localStorage.getItem('selectedAnswers') || '[]'
+        );
         if (!encryptedData) {
           throw new Error('No quiz data found');
         }
@@ -69,14 +72,19 @@ const QuizPage: React.FC<QuizProps> = () => {
         const decryptedData = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
 
         setQuestionsAndAnswers(
-          decryptedData.results.map((questionObject: QuestionObject, index: number) => {
-            return {
-              question: questionObject.question,
-              shuffledAnswers: shuffle([...questionObject.incorrect_answers, questionObject.correct_answer]),
-              correctAnswer: questionObject.correct_answer,
-              selectedAnswer: savedAnswers[index] || '',
-            };
-          })
+          decryptedData.results.map(
+            (questionObject: QuestionObject, index: number) => {
+              return {
+                question: questionObject.question,
+                shuffledAnswers: shuffle([
+                  ...questionObject.incorrect_answers,
+                  questionObject.correct_answer,
+                ]),
+                correctAnswer: questionObject.correct_answer,
+                selectedAnswer: savedAnswers[index] || '',
+              };
+            }
+          )
         );
 
         setLoading(false);
@@ -91,18 +99,26 @@ const QuizPage: React.FC<QuizProps> = () => {
   }, []);
 
   const updateAnswer = (currentQuestion: string, answer: string) => {
-    const updatedQuestionsAndAnswers = questionsAndAnswers.map((questionObject: QuestionAndAnswer) => {
-      return questionObject.question === currentQuestion ? { ...questionObject, selectedAnswer: answer } : questionObject;
-    });
+    const updatedQuestionsAndAnswers = questionsAndAnswers.map(
+      (questionObject: QuestionAndAnswer) => {
+        return questionObject.question === currentQuestion
+          ? { ...questionObject, selectedAnswer: answer }
+          : questionObject;
+      }
+    );
 
     setQuestionsAndAnswers(updatedQuestionsAndAnswers);
     // Save updated answers to localStorage
-    const selectedAnswers = updatedQuestionsAndAnswers.map((q) => q.selectedAnswer);
+    const selectedAnswers = updatedQuestionsAndAnswers.map(
+      (q) => q.selectedAnswer
+    );
     localStorage.setItem('selectedAnswers', JSON.stringify(selectedAnswers));
   };
 
   const submitAnswers = () => {
-    const notAllAnswered = questionsAndAnswers.some((questionObject) => questionObject.selectedAnswer === '');
+    const notAllAnswered = questionsAndAnswers.some(
+      (questionObject) => questionObject.selectedAnswer === ''
+    );
 
     setWarning(notAllAnswered);
 
@@ -141,7 +157,11 @@ const QuizPage: React.FC<QuizProps> = () => {
     navigate('/dashboard');
   };
 
-  const unansweredQuestions = questionsAndAnswers.map((questionObject, index) => (questionObject.selectedAnswer === '' ? index + 1 : null)).filter((questionNumber) => questionNumber !== null);
+  const unansweredQuestions = questionsAndAnswers
+    .map((questionObject, index) =>
+      questionObject.selectedAnswer === '' ? index + 1 : null
+    )
+    .filter((questionNumber) => questionNumber !== null);
 
   const questionElements = questionsAndAnswers.map((questionObject, index) => {
     const questionNumber = index + 1;
@@ -173,55 +193,86 @@ const QuizPage: React.FC<QuizProps> = () => {
   return (
     <>
       {loading ? (
-        <p className="flex items-center justify-center h-screen text-xl">Loading ...</p>
+        <p className="flex items-center justify-center h-screen text-xl">
+          Loading ...
+        </p>
       ) : (
         <>
           <div className="max-w-5xl mx-auto flex flex-col justify-center items-center mt-10 space-y-2 mb-[50px]">
             <div className="container px-2 md:px-4 py-2 mx-auto">
               <div id="google_translate_element"></div>
-              <h1 className="text-3xl !font-bold uppercase text-center mb-11 text-[#f9a826]">Quiz Page</h1>
+              <h1 className="text-3xl !font-bold uppercase text-center mb-11 text-[#f9a826]">
+                Quiz Page
+              </h1>
               <GoogleTranslate />
               {questionElements}
             </div>
             {/* {warning && <p className="text-red-500 text-lg font-semibold">Please answer all questions before submit!</p>} */}
             {warning && (
-              <div>
-                <p className="text-red-500 text-lg font-semibold">Please answer all questions before submit!</p>
+              <div className="px-7">
                 <div>
-                  <p className="text-red-500 font-semibold">
+                  <p className="text-red-500 font-semibold mt-3 mb-5">
                     {unansweredQuestions.length > 0 ? (
-                      <div>
+                      <>
+                        <p className="text-red-500 text-lg font-semibold">
+                          Please answer all questions before submit!
+                        </p>
                         Unanswered Questions:
-                        {unansweredQuestions.map((num, index) => (
-                          <React.Fragment key={num}>
-                            {index > 0 && ', '}
-                            <a href={`#question-${num}`} className="text-blue-500 underline">
-                              {num}
-                            </a>
-                          </React.Fragment>
-                        ))}
-                      </div>
-                    ) : null}
+                        <div className="mt-7 flex flex-wrap gap-10 justify-start">
+                          {unansweredQuestions.map((num, index) => (
+                            <div key={num} className="">
+                              {index > 0 && ' '}
+                              <a
+                                href={`#question-${num}`}
+                                className="rounded-lg underline bg-red-500 w-[3rem] h-[3rem] grid place-items-center text-lg block m-1 text-white"
+                              >
+                                {num}
+                              </a>
+                            </div>
+                          ))}
+                        </div>
+                      </>
+                    ) : (
+                      <>
+                        <p className="text-green-500 text-xl font-bold">
+                          All question already answered, you can submit now
+                        </p>
+                      </>
+                    )}
                   </p>
                 </div>
               </div>
             )}
 
             {questionsAndAnswers.length > 0 && !showResult ? (
-              <button onClick={submitAnswers} className="bg-[#f9a826] hover:bg-yellow-600 font-bold text-white rounded-md mt-11 px-6 py-3 w-fit">
-                Submit
-              </button>
+              <>
+                <button
+                  onClick={submitAnswers}
+                  className="bg-[#f9a826] hover:bg-yellow-600 font-bold text-white rounded-md mt-11 px-6 py-3 w-fit mb-7"
+                >
+                  Submit
+                </button>
+                <br />
+                <br />
+              </>
             ) : null}
 
             {showResult && (
               <div className="w-full flex gap-3 rounded-lg justify-center py-11">
-                <p className="p-3 bg-yellow-500 rounded-lg">Total Question: 15</p>
-                <p className="p-3 bg-green-500 rounded-lg">Correct Answer: {numCorrectAnswer}</p>
+                <p className="p-3 bg-yellow-500 rounded-lg">
+                  Total Question: 15
+                </p>
+                <p className="p-3 bg-green-500 rounded-lg">
+                  Correct Answer: {numCorrectAnswer}
+                </p>
               </div>
             )}
 
             {showResult && (
-              <button onClick={resetQuiz} className="bg-blue-500 hover:bg-blue-800 font-bold text-white rounded-md px-4 py-2">
+              <button
+                onClick={resetQuiz}
+                className="bg-blue-500 hover:bg-blue-800 font-bold text-white rounded-md px-4 py-2"
+              >
                 Go to Dashboard
               </button>
             )}
